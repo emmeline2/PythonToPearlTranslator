7bc127015b8fcbece9af14965d8572edb7f77678
emmeline2 <elp65@case.edu>
2017-09-30 16:58:30 +1000

    Create demo04.py

---
+++ demo04.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# conversions 
+
+#temperature
+tempDegrees = 32
+#doesn't handle text + variable print statments
+print("The temperature in degrees Fahrenheit is: ")
+print(tempDegrees)
+tempCelcius = tempDegrees - 32
+tempCelcius = tempcelcius * 0.5556 
+print("The temperature in degrees Celsius is: ")
+print(tempCelcius)
+
+
+#weight
+kgs = 70
+print("The weight in kilograms is: ") 
+print(kgs)
+lbs = kg * 2.2046   
+print("The weight in pounds is: ")
+print(lbs)
