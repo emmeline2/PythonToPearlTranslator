86d1a7d47bab2a9f377d5ff439cad739d5841235
emmeline2 <elp65@case.edu>
2017-09-30 16:57:01 +1000

    Create demo00.py

---
+++ demo00.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#based of of answer5.py from sample scripts 
+
+#if loop test
+answer = 27
+if answer > 0: answer = answer + 2
+if answer == 27: answer = answer - 3
+print(answer)
+
+#simple operator tests 
+x = 4 
+x = x + 3
+x = x % 2
+x = 3 * x
+x = 27 - x
