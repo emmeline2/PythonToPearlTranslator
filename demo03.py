b322fe1639afc834db8e777fda9e7264e2e898af
emmeline2 <elp65@case.edu>
2017-09-30 16:58:12 +1000

    Create demo03.py

---
+++ demo03.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# subset 2 tests using if loops and while loops 
+
+A = 18
+if A > 0: A = A + 1
+if A == 27: A = A - 2
+print(A)
+print("Now onto the while loop")
+while A < 36: A = A + 7
+print(A)
+print("^the value of A") 
+
+
+#random print tests 
+print("\n") 
+print("here is a tab \t")
+print("$") 
+print("A") 
+print("A $A") 
+print(A)
+
+#doesn't translate totally correctly
