61841221f5e418b35cb06568466217944a2d5b63
emmeline2 <elp65@case.edu>
2017-09-30 16:57:53 +1000

    Create demo02.py

---
+++ demo02.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#subset 2 based tests (while loops) 
+
+value = 1
+while value < 36: value = value + 1
+print(value)
+
+
+
+#2nd while loop
+value = 0
+while value < 32: value = value + 2;print(value)
+
+
+#3rd while loop
+value = 3
+while value < 42: value = value + 7;print(value)
+value = value % 2 
+print(value)
