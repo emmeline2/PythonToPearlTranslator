7d726937a647d9b8ebf9ce05529fd95789e904fb
emmeline2 <elp65@case.edu>
2017-09-30 16:57:24 +1000

    Create demo01.py

---
+++ demo01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#print and variable demo 
+
+#simple print tests
+print("hello world")
+print("DOEs CaSE mAtteR")
+print("there? are, so. many! symbols$")
+print("Extra     Spaces   	")
+
+#variable declaration tests
+amkew2m0 = 6
+msk1 = 1
+answer = amkew2m0 * msk1
+print(answer)
+
+answer2 = 2 / 1 * 3 - 9
+print("answer two is: ")
+print(answer2)
+
+multiply1 = 10
+multipy2 = 6
+print(multiply1 * multiply2)
